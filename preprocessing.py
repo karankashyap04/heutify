dad0e3caa9f51ba6dba78d1f6f8c5fc72095eca1
karankashyap04 <karankashyap@gmail.com>
2022-12-11 02:15:39 -0500

    docs: Add documentation for preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -3,14 +3,26 @@ from PIL import Image
 from skimage import color
 import os
 
+"""
+Class containing the code used to preprocess the images from the Intel Image
+Classification Dataset.
+"""
 class Preprocessor:
     def __init__(self):
+        """
+        Constructor for the Preprocessor class.
+        Used to initialize filepaths needed for the preprocessor to work.
+        """
         self.INTEL_BASE_DIR = "data/intel-image-classification-dataset/"
         self.INTEL_SUBDIRECTORIES = ["seg_pred/seg_pred/", "seg_test/seg_test/", "seg_train/seg_train/"]
         self.INTEL_TRAIN_TEST_SUBDIRECTORIES = ["buildings/", "forest/", "glacier/", "mountain/", "sea/", "street/"]
         self.WRITE_DIR = "preprocessed_data/intel-dataset/"
 
     def get_image_paths(self):
+        """
+        Creates a list of filepaths for all images to be preprocessed based on 
+        the folder filepaths initialized in the constructor.
+        """
         image_paths = []
         for subdirectory in self.INTEL_SUBDIRECTORIES:
             if subdirectory == "seg_pred/seg_pred/":
@@ -23,9 +35,27 @@ class Preprocessor:
         return image_paths
 
     def preprocess_images(self, input_images):
+        """
+        Preprocesses a numpy array of images represented in the Lab colorspace by
+        isolating the L channel and returning it (returns the images with the
+        a and b channel values removed).
+
+        params:
+        input_images -> a numpy array of image data represented in the Lab
+                        color space.
+        """
         return input_images[:,:,:,0]
 
     def get_data(self, image_paths):
+        """
+        Reads the data from all the provided image filepaths. This image data
+        is converted from the RGB color space to the Lab colorspace. Then the
+        data is preprocessed and the preprocessed inputs and labels are returned.
+
+        params:
+        image_paths -> a list of filepaths to all the images whose data needs
+                        to be read and preprocessed.
+        """
         X = []
         for i, image_path in enumerate(image_paths):
             image = Image.open(image_path)
@@ -41,11 +71,24 @@ class Preprocessor:
         return self.preprocess_images(input_images=X), y
     
     def write_data(self, X, y):
+        """
+        Takes the preprocessed input and label data (two numpy arrays) and saves
+        this data as .npy files (within the directory specified in self.WRITE_DIR).
+
+        params:
+        X -> a numpy array containing the input data (black and white images i.e.
+                images with only the L channel)
+        y -> a numpy array containing the ground truth images
+        """
         np.save(self.WRITE_DIR + "inputs", X)
         np.save(self.WRITE_DIR + "labels", y)
 
 
 if __name__ == '__main__':
+    """
+    Main method for this file. Used to instantiate the Preprocessor class and
+    run the code in it to preprocess the images.
+    """
     preprocessor = Preprocessor()
     image_paths = preprocessor.get_image_paths()
     X, y = preprocessor.get_data(image_paths=image_paths)
